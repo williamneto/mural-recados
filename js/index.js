63957019151f7863cc0416c07a191f3b9d2e3268
Flavio Nunes <flavioborgesnunes@gmail.com>
2022-01-28 23:49:23 -0300

    exibindo as mensagens em tabela

--- js/index.js
+++ js/index.js
@@ -1,5 +1,28 @@
 
 $(document).ready(function(){
+    function exibirMensagens(){
+        var mensagenssalvas = []
+        if (localStorage.getItem("mensagens")){
+            mensagenssalvas = JSON.parse(
+                localStorage.getItem("mensagens")
+            )
+        }
+        console.log(mensagenssalvas)
+        if (mensagenssalvas.length > 0){
+            $("#nenhumaMensagem").css("display", "none")
+        }
+        $("#tbody").html("")
+        for (let x = 0; x < mensagenssalvas.length; x++ ){ //x++ é o mesmo que x = x + 1
+            var usuario = mensagenssalvas[x].usuario
+            var texto = mensagenssalvas[x].texto
+            $("#tbody").append(
+                `<tr> <td> ${usuario}</td><td>${texto}</td></tr>`
+            )
+        }
+        
+
+    }
+
     $("#send").click(function(){
         var mensagenssalvas = []
         if (localStorage.getItem("mensagens")){
@@ -14,6 +37,12 @@ $(document).ready(function(){
         }
         var mensagemTxt = JSON.stringify([ ...mensagenssalvas, mensagem])
         localStorage.setItem("mensagens",mensagemTxt)
+
+        exibirMensagens()
+        $("#username").val("")
+        $("#mensagem").val("")
     })
+    exibirMensagens()
+
     
 })
