10d7ea7729a391b7e5f3a99277fab995a0afd74e
France Lima <francefilo@gmail.com>
2022-01-28 22:40:45 -0300

    puxou a mensagem no localstorage e salvou a nova mensagem no localstorage

--- js/index.js
+++ js/index.js
@@ -1,13 +1,18 @@
 
 $(document).ready(function(){
     $("#send").click(function(){
-        var usuario = $('#username').val()
-        var texto = $('#mensagem').val()
+        var mensagenssalvas = []
+        if (localStorage.getItem("mensagens")){
+            mensagenssalvas = JSON.parse(
+                localStorage.getItem("mensagens")
+            )
+        }
+        
         var mensagem = {
             "usuario": $('#username').val(),
             "texto": $('#mensagem').val()
         }
-        var mensagemTxt = JSON.stringify([mensagem])
+        var mensagemTxt = JSON.stringify([ ...mensagenssalvas, mensagem])
         localStorage.setItem("mensagens",mensagemTxt)
     })
     
